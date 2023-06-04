69fda480ec4df311964c1de326606a06913eb617
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-06-04 16:32:06 +0600

    Update autosetup.bat

--- autosetup.bat
+++ autosetup.bat
@@ -1,20 +1,3 @@
-@echo off
-
-
-set PYTHON_INSTALL_PATH=C:\Python39
-
-
-
-
-
-echo Installing Python...
-start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=%PYTHON_INSTALL_PATH%
-
-echo Cleaning up...
-del python-installer.exe
-
-echo Python installation completed.
-
 
 pip install beautifulsoup4
 pip install requests
