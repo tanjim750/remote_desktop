e7c93198aebc91610e5f2fccde4bfca72f27b848
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-06-04 16:02:43 +0600

    Update autosetup.bat

--- autosetup.bat
+++ autosetup.bat
@@ -1,21 +1,19 @@
 @echo off
 
-set PYTHON_VERSION=3.9.7
-set PYTHON_INSTALL_PATH=C:\Python39
-set PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
 
-echo Downloading Python installer...
-powershell -Command "Invoke-WebRequest -Uri '%PYTHON_DOWNLOAD_URL%' -OutFile 'python-installer.exe'"
+set PYTHON_INSTALL_PATH=C:\Python39
+
+
+
+
 
 echo Installing Python...
-powershell -Command "Start-Process -FilePath 'python-installer.exe' -ArgumentList '/quiet', 'InstallAllUsers=1', 'PrependPath=1', 'TargetDir=%PYTHON_INSTALL_PATH%' -Wait"
+start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=%PYTHON_INSTALL_PATH%
 
 echo Cleaning up...
 del python-installer.exe
 
 echo Python installation completed.
-
-
 
 
 pip install beautifulsoup4
