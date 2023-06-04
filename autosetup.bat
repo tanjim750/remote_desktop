4a8ba96bc591399b29eafceca07458f8a3a01c12
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-06-04 09:50:49 +0600

    Update autosetup for download and install python

--- autosetup.bat
+++ autosetup.bat
@@ -1,3 +1,20 @@
+@echo off
+
+set PYTHON_VERSION=3.9.7
+set PYTHON_INSTALL_PATH=C:\Python39
+set PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
+
+echo Downloading Python installer...
+curl -o python-installer.exe %PYTHON_DOWNLOAD_URL%
+
+echo Installing Python...
+start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=%PYTHON_INSTALL_PATH%
+
+echo Cleaning up...
+del python-installer.exe
+
+echo Python installation completed.
+
 pip install beautifulsoup4
 pip install requests
 pip install pyautogui
